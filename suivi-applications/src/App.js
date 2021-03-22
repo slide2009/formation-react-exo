6f2465bc618149bcbfe3af036b38aa9d6ab61fc8
Minh Nguyen <minh.nguyen@insee.fr>
2021-03-22 14:55:23 +0100

    setState as props

--- suivi-applications/src/App.js
+++ suivi-applications/src/App.js
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 import Formulaire from './components/applications/Formulaire';
 import MonComposant from './components/applications/MonComposant';
 
-const App = () => <div className="App">
-  Mon application
-  <Formulaire/>
-</div>
+const App = () => {
+
+  const[compteur, setCompteur] = useState(0);
+
+  return (
+    <div className="App">
+      Mon application
+      <Formulaire setCompteur={setCompteur}/>
+      <div>{compteur}</div>
+    </div>
+  )
+}
 
 export default App;
--- suivi-applications/src/components/applications/Formulaire.js
+++ suivi-applications/src/components/applications/Formulaire.js
@@ -1,17 +1,30 @@
 import React, { useState } from 'react'
 
-const Formulaire = () => {
-    const [nom, ..] = useState('');
-    const [version, ..]
+const Formulaire = ({setCompteur}) => {
+    const [nom, setNom] = useState('');
+    const [version, setVersion] = useState('');
+    const [etat, setEtat] = useState('ACTIVE');
     
+    const enregistrer = e => {
+        e.preventDefault();
+        console.log(` ${nom} ${version} ${etat}`)
+        setCompteur(compteur => compteur+1);
+    }
+
     return <form>
-        <input/>
-        <input/>
-        <select onChange={e => set() }>
+        <label>
+            Nom :
+            <input type="text" onChange= { e => setNom(e.target.value)} value={nom}/> 
+        </label>
+        <label>
+            Version :
+            <input type="text" onChange= { e => setVersion(e.target.value)} value={version}/> 
+        </label>
+        <select onChange={e => setEtat(e.target.value) }>
             <option value="ACTIVE">Active</option>
             <option value="OBSOLETE">Obsolète</option>
         </select>
-        <button onClick = {() => {console.log()} }>Enreg</button>
+        <button onClick = {enregistrer}>Enregistrer</button>
     </form>
 }
 
