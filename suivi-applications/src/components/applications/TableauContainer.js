348a5500a1be725c0ca56e2d666ee53b519f7fab
Minh NGUYEN <minh.nguyen@insee.fr>
2021-10-11 07:30:06 +0200

    nettoyage pre-formation

--- suivi-applications/src/components/applications/TableauContainer.js
+++
@@ -1,6 +0,0 @@
-import { connect } from 'react-redux';
-import Tableau from './Tableau';
-
-const mapStateToProps = storeState => ({ compteur : storeState.compteur});
-
-export default connect(mapStateToProps)(Tableau);
--- suivi-applications/src/components/applications/actions.js
+++
@@ -1,5 +0,0 @@
-export const MODIFIER_COMPTEUR = "MODIFIER_COMPTEUR"
-export const modifierCompteur = payload => {
-    console.log(payload)
-    return {type : MODIFIER_COMPTEUR, payload };
-}
